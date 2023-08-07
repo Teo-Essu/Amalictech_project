655d122f4283c06c4bc83685110a30f4ae327d12
Teo-Essu <tnessuman@gmail.com>
2023-08-07 06:02:42 +0000

    modified app title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = ({onSearch}) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
-                <a className="navbar-brand" href="/home">My App</a>
+                <a className="navbar-brand" href="/home">My File_Server</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
